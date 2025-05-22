30b2d768b082b4439f932326e3f3c9f34bc27e13
Vander1221 <fogoraww@mail.ru>
2025-05-22 11:47:26 +0300

    Update Exercise3.cs

--- Code-run/Exercise3.cs
+++ Code-run/Exercise3.cs
@@ -1,30 +1,30 @@
-﻿using System; //решает задачу подсчета количества возможных комбинаций знаков "движение по полосам" на перекрестке
+﻿using System;
 
-class Program
+public class TagTime
 {
-    static void Main()
+    public static void Main(string[] args)
     {
-        // Ввести два числа через пробел: m (направления) и n (полосы)
-        // Например: "4 2"
-        string[] input = Console.ReadLine().Split();
-        int m = int.Parse(input[0]); // количество возможных направлений (2 ≤ m ≤ 50)
-        int n = int.Parse(input[1]); // количество полос движения (1 ≤ n ≤ 15)
+        int n = int.Parse(Console.ReadLine());
+        long[] t = new long[n];
+        long totalTime = 0;
 
-        long[,] dp = new long[m + 1, n + 1];
+        if (n >= 1)
+        {
+            t[0] = 1;
+            totalTime += t[0];
+        }
+        if (n >= 2)
+        {
+            t[1] = 1;
+            totalTime += t[1];
+        }
 
-        // Базовые случаи для 1 полосы
-        for (int k = 1; k <= m; k++)
-            dp[k, 1] = 1;
+        for (int i = 2; i < n; i++)
+        {
+            t[i] = t[i - 1] + t[i - 2];
+            totalTime += t[i];
+        }
 
-        for (int j = 2; j <= n; j++)
-            for (int i = 1; i <= m; i++)
-                for (int k = 1; k <= i; k++)
-                    dp[i, j] += dp[k, j - 1];
-
-        long result = 0;
-        for (int k = 1; k <= m; k++)
-            result += dp[k, n];
-
-        Console.WriteLine(result);
+        Console.WriteLine(totalTime);
     }
 }
